896fdcf138c332ad2cbe95d0403c12ec41ff87c7
tushgaurav <tushar.gaurav416@gmail.com>
2022-01-06 19:56:10 +0530

    added tower of hanoi problem

---
+++ TowerofHanoi.py
@@ -0,0 +1,11 @@
+# recursive solution
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
+    if n == 0:
+        return
+    TowerOfHanoi(n - 1, from_rod, aux_rod, to_rod)
+    print("Move disk", n, "from rod", from_rod, "to rod", to_rod)
+    TowerOfHanoi(n - 1, aux_rod, to_rod, from_rod)
+
+
+n = 3
+TowerOfHanoi(3, "A", "B", "C")
